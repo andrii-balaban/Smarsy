4de53bdd9dc113bc0f5ee30d8a47851459a7a74c
Andrii Balaban <andriy.balaban@gmail.com>
2017-10-07 16:15:00 +0300

    Fix interface implementation

--- Smarsy.Test/DataBaseStub.cs
+++ Smarsy.Test/DataBaseStub.cs
@@ -23,7 +23,7 @@ namespace Smarsy.Test
             throw new NotImplementedException();
         }
 
-        public void UpserStudentAllLessonsMarks(IList<LessonMark> marks)
+        public void UpserStudentAllLessonsMarks(SmarsyStudent student, IList<LessonMark> marks)
         {
             throw new NotImplementedException();
         }
